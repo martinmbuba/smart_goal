91d54964dff1f17c058721e9f6b5c5a428738a09
martinmbuba <martinmbuba4@gmail.com>
2025-07-22 21:22:43 +0300

    fixed server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
 
+// Enable CORS for all origins
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
